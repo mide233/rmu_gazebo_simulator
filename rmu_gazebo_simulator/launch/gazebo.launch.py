3e15b9060c78b18503a57476ce669d920578d9c5
LihanChen2004 <74599182+LihanChen2004@users.noreply.github.com>
2025-11-03 09:27:25 +0800

    🐎 ci: update ci and copyright (#7)
    * 🐎 ci: update ci and copyright
    
    * 🐎 ci: remove push trigger from build and test workflow
    
    * 🎈 perf(gazebo.launch.py): Remove useless `AppendEnvironmentVariable`
    
    * 🎈 perf: remove `#!/usr/bin/python3` in scripts

--- rmu_gazebo_simulator/launch/gazebo.launch.py
+++ rmu_gazebo_simulator/launch/gazebo.launch.py
@@ -1,9 +1,22 @@
+# Copyright 2025 Lihan Chen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (
-    AppendEnvironmentVariable,
     DeclareLaunchArgument,
     IncludeLaunchDescription,
 )
@@ -30,17 +43,6 @@ def generate_launch_description():
         "ign_config_path",
         default_value=os.path.join(pkg_simulator, "resource", "ign", "gui.config"),
         description="Path to the Ignition Gazebo GUI configuration file",
-    )
-
-    # Set Gazebo plugin and resource path
-    append_enviroment_worlds = AppendEnvironmentVariable(
-        name="GAZEBO_PLUGIN_PATH",
-        value=os.path.join(pkg_simulator, "resource", "worlds"),
-    )
-
-    append_enviroment_models = AppendEnvironmentVariable(
-        name="IGN_GAZEBO_RESOURCE_PATH",
-        value=os.path.join(pkg_simulator, "resource", "models"),
     )
 
     # Launch Gazebo simulator
@@ -72,8 +74,6 @@ def generate_launch_description():
 
     ld.add_action(declare_world_sdf_path)
     ld.add_action(declare_ign_config_path)
-    ld.add_action(append_enviroment_worlds)
-    ld.add_action(append_enviroment_models)
     ld.add_action(gazebo)
     ld.add_action(robot_ign_bridge)
 
